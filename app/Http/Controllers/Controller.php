a16af28dc36532b36cbc7ab3193150bb9417ca7f
DOUMEFIO <108420187+DOUMEFIO@users.noreply.github.com>
2023-03-02 19:11:26 +0100

    api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,44 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+/**
+     *  @OA\Get(
+     *      path="ROUTE API",
+     *      summary="Title",
+     *      security={{"bearer_token":{}}},
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Donec sollicitudin molestie malesuada."
+     *      ),
+     *      @OA\Response(
+     *          response="default",
+     *          description="An error has occurred."
+     *      )
+     *  )
+     */
+ /** @OA\Info(
+ *      version="1.0.0",
+ *      title="Swagger Travail",
+ *      description="Implementation de swagger en laravel",
+ * ),
+ *
+ *    @OA\Server(
+  *    url="https://app.payplus.africa/pay/v01/redirect/checkout-invoice",
+  * )
+
+ *  @OA\SecurityScheme(
+ *      type="http",
+ *      description="Authentication Bearer Token",
+ *      name="Authentication Bearer Token",
+ *      in="header",
+ *      scheme="bearer",
+ *      bearerFormat="JWT",
+ *      securityScheme="apiAuth",
+*)
+*/
+
+
 
 class Controller extends BaseController
 {
